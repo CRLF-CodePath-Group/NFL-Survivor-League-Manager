ff373a48ecc399f4afbf2e85a1c0a53ed536ea7e
Jacob Frick <jacobfrick@Jacobs-MacBook-Air-2.local>
2018-10-19 18:58:09 -0700

    Init xcode project settings

---
+++ NFL-Survivor-League-Manager/NFL-Survivor-League-Manager/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFL-Survivor-League-Manager
+//
+//  Created by Jacob Frick on 10/19/18.
+//  Copyright © 2018 Jacob Frick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
