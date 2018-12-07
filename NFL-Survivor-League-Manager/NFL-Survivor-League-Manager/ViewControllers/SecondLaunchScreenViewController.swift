5d1e15248903219742ec158765664e29579f3df1
Jacob Frick <jacobfrick@Jacobs-MacBook-Air-3.local>
2018-12-06 17:00:09 -0800

    Added new init view controller to extend launch. This controller will load the NFL Schedule into a global variable to speed up loading times for picker

---
+++ NFL-Survivor-League-Manager/NFL-Survivor-League-Manager/ViewControllers/SecondLaunchScreenViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondLaunchScreenViewController.swift
+//  NFL-Survivor-League-Manager
+//
+//  Created by Jacob Frick on 12/6/18.
+//  Copyright © 2018 Jacob Frick. All rights reserved.
+//
+
+import UIKit
+
+class SecondLaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NFLAPIManager.grabFullNFLSeason { (schedule, error) in
+            if let schedule = schedule {
+                NFLSchedule = schedule
+                self.performSegue(withIdentifier: "toMainPage", sender: nil)
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
+extension UIViewController {
+    
+}
